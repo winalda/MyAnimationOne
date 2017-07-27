c15ddd222b85cef9cfedaa5686133ab69dc65b8e
winalda <alberto@kokonutstudio.com>
2017-07-26 23:45:37 -0500

    Aprendiendo unity
    Estes es un proyecto creado con la única interino de aprender las
    herramientas de unity2D

---
+++ Assets/Scripts/BaseMain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (-1,GetComponent<Rigidbody2D>().velocity.y);
+	}
+}
---
+++ Assets/Scripts/ChararcterMain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChararcterMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log("x = "+GetComponent<Rigidbody2D>().velocity.x);
+		Debug.Log("y = "+GetComponent<Rigidbody2D>().velocity.y);
+
+		if (Input.GetKey(KeyCode.Space)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, 3);
+		} else {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (1,GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+}
